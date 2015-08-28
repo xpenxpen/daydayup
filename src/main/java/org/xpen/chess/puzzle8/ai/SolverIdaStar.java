4d7b519281afdea0fd86de6df3b4c2073faf1d68
xpenxpen <xpenxpen@163.com>
2015-08-28 09:49:05 +0800

    棋类游戏，人工智能

--- src/main/java/org/xpen/chess/puzzle8/ai/SolverIdaStar.java
+++ src/main/java/org/xpen/chess/puzzle8/ai/SolverIdaStar.java
@@ -37,6 +37,8 @@ public class SolverIdaStar extends Solver {
         //初始阶段曼哈顿距离，表示最少要移动步数
         int maxThreadhold = initial.priority();
         
+        //迭代加深A*搜索，比如第一次迭代搜索深度最大为30，搜不到下一次迭代深度为31，直到搜到为止。越到后面越慢。
+        //虽然有一些重复的搜索，比如搜31时前面30又要重搜一遍，但还是比A*要快，而且省内存。
         while (true) {
             System.out.println("Start finding goal at maxThreadhold=" + maxThreadhold);
             queue.clear();
--- src/main/java/org/xpen/chess/puzzle8/ai/SolverWidthFirst.java
+++ src/main/java/org/xpen/chess/puzzle8/ai/SolverWidthFirst.java
@@ -1,7 +1,5 @@
 package org.xpen.chess.puzzle8.ai;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
